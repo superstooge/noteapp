a276cd012baa0b7b79ef136f2cd0bcf8c20c1cf9
Federico.a.carbone <federico.a.carbone@accenture.com>
2018-11-25 03:55:46 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import './App.scss';
-import Home from './containers/Home';
-import plus from './static/add.svg';
-import logo from './static/logo.png';
-import { actionsTypes, dispatchAction } from './actions/actions';
-import 'react-notifications/lib/notifications.css';
+import React, { Component } from "react";
+import "./App.scss";
+import Home from "./containers/Home";
+import plus from "./static/add.svg";
+import logo from "./static/logo.png";
+import { actionsTypes, dispatchAction } from "./actions/actions";
+import "react-notifications/lib/notifications.css";
 
 class App extends Component {
   createNewNoteAction() {
@@ -20,7 +20,7 @@ class App extends Component {
           <span className="app-title">Note app</span>
           <div className="button-create" onClick={this.createNewNoteAction}>
             <span>
-                     <img src={plus} alt="create new note" />
+              <img src={plus} alt="create new note" />
             </span>
             create
           </div>
